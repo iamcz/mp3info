7ec73a4687b7d4259dd6e3abddaa3b775fba6611
Charles Zahn <charlesazahn@gmail.com>
2018-01-16 02:26:58 -0500

    add mp3 file, extract command line args

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(int argc, char * argv[]) {
   cout << "main()" << endl;
+  if (argc != 2) {
+    cerr << "Error: must enter a file name" << endl;
+    return -1;
+  }
+  string filename = argv[1];
+  cout << "Filename: " << filename << endl;
   return 0;
 }
