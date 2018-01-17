1f8c7319831881dfedfd01ea613a068ecd3263b9
Charles Zahn <charlesazahn@gmail.com>
2018-01-17 10:31:19 -0500

    add classes (WIP)

---
+++ ID3Info.cpp
@@ -0,0 +1,6 @@
+#include "ID3Info.hpp"
+
+ID3Info::ID3Info(
+    unsigned int m_version,
+    unsigned int r_number
+) : major_version(m_version), rev_number(r_number) {}
---
+++ ID3Info.hpp
@@ -0,0 +1,13 @@
+#ifndef ID3Info_HPP
+#define ID3Info_HPP
+
+class ID3Info {
+  private:
+    unsigned int major_version;
+    unsigned int rev_number;
+  public:
+    ID3Info(unsigned int m_version, unsigned int r_number);
+    char * verZZZZZZZ
+};
+
+#endif
